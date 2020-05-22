248a3c9332bc77dccad4a9aadc3a8b79aed5d325
mitant127 <55878970+mitant127@users.noreply.github.com>
2020-05-22 22:40:10 +0300

    Formalisation PEP8

--- Problem_1.py
+++ Problem_1.py
@@ -1,14 +1,14 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5,
-# we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of
-# all the multiples of 3 or 5 below 1000.
+"""If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of
+all the multiples of 3 or 5 below 1000."""
 
-n = 0
-n2 = 1000
-sum_n = 0
+N = 0
+N2 = 1000
+SUM_N = 0
 
-while n < n2:
-    if n % 3 == 0 or n % 5 == 0:
-        sum_n += n
-        #print(sum_n, n)
-    n += 1
-print(f"Summ = {sum_n}")
+while N < N2:
+    if N % 3 == 0 or N % 5 == 0:
+        SUM_N += N
+        # print(SUM_N, N)
+    N += 1
+print(f"Summ = {SUM_N}")
