18ea03697b2c5744b1baa35df787ad6357c247db
mitant127 <55878970+mitant127@users.noreply.github.com>
2020-05-22 22:46:11 +0300

    Appliend algorithms

--- Problem_3.py
+++ Problem_3.py
@@ -1,23 +1,22 @@
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143 ?
+"""The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?"""
 
-n = 600851475143
-n2 = 1
-i = 2
+# from math import sqrt, ceil
 
-print(n)
-while True:
+NUMB = 600851475143
+FACTOR = 2
+PRIME_ARY = []
+DIVIDER_ARY = []
 
-    if n % i == 0:
+while FACTOR <= NUMB:
+    for i in PRIME_ARY:
+        if FACTOR % i == 0:
+            break
+    else:
+        PRIME_ARY.append(FACTOR)
+        if NUMB % FACTOR == 0:
+            DIVIDER_ARY.append(FACTOR)
+            NUMB = NUMB / FACTOR
+    FACTOR += 1
 
-        if n2 < i:
-            n2 = i
-            n = n / i
-            i = 1
-
-            if n == 1:
-                break
-
-    i += 1
-
-print(f"Max divider = {n2}")
+print(f"Max divider = {max(PRIME_ARY)}")
